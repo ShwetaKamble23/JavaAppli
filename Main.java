666838ca8ae0ccf5c1decf0af18ff160598b20f7
ShwetaKamble23 <kambleshweta0723@gmail.com>
2022-07-04 15:43:33 +0530

    Added java demo application files

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+  public static void main(String[] args) {
+	  System.out.println(checkIfTnputIsAnEvenNumber(122));
+	
+}
+
+static boolean checkIfTnputIsAnEvenNumber(int number) {
+	
+	return number % 2 == 0;
+}
+}
